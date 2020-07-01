fad705cb4c3215a1133064b7cf7a1f82801d4675
calthompson <54155266+calthompson@users.noreply.github.com>
2020-07-01 15:19:24 -0400

    Initialized

---
+++ prayer_requests/_vti_cnf/current_prayer_requests.php
@@ -0,0 +1,22 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|08 Aug 2019 18:32:26 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|TCM\\cthompson
+vti_modifiedby:SR|TCM\\cthompson
+vti_timecreated:TR|12 Jul 2019 15:33:08 -0000
+vti_title:SR|Prayer Requests | The City Mission Intranet
+vti_backlinkinfo:VX|prayer_requests/prayer.html
+vti_nexttolasttimemodified:TR|05 Aug 2019 17:40:27 -0000
+vti_cacheddtm:TX|08 Aug 2019 18:32:26 -0000
+vti_filesize:IR|3351
+vti_cachedtitle:SR|Prayer Requests | The City Mission Intranet
+vti_cachedbodystyle:SR|<body>
+vti_cachedlinkinfo:VX|G|../master.dwt Q|../styles/style1.css H|http://thecitymission.org S|../images/logo.png H|../ H|../about/about.html H|../newsletters/newsletters.html H|../church_and_community/church_community_home.php H|../classifieds/classifieds.html H|../human_resources/human_resources.php H|prayer.html H|../staff_gallery/staff.html H|prayer.html S|../images/back-button.png H|https://g.page/thecitymission H|mailto:jbraden@thecitymission.org
+vti_cachedsvcrellinks:VX|FGUS|master.dwt FQUS|styles/style1.css NHHS|http://thecitymission.org FSUS|images/logo.png WHUS|index.html FHUS|about/about.html FHUS|newsletters/newsletters.html FHUS|church_and_community/church_community_home.php FHUS|classifieds/classifieds.html FHUS|human_resources/human_resources.php FHUS|prayer_requests/prayer.html FHUS|staff_gallery/staff.html FHUS|prayer_requests/prayer.html FSUS|images/back-button.png NHSS|https://g.page/thecitymission NHUS|mailto:jbraden@thecitymission.org
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=utf-8 viewport width=device-width,\\ initial-scale=1.0
+vti_charset:SR|utf-8
+vti_syncwith_localhost\\\\\\mra-sql1\\missionnet\\sandbox missionnet///mra-sql1/missionnet/sandbox missionnet:TR|08 Aug 2019 18:32:26 -0000
---
+++ staff_gallery/_vti_cnf/photo_gallery.php
@@ -0,0 +1,22 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|09 Aug 2019 15:08:29 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|staff_gallery/staff.html
+vti_author:SR|TCM\\cthompson
+vti_modifiedby:SR|TCM\\cthompson
+vti_timecreated:TR|12 Jul 2019 15:29:34 -0000
+vti_title:SR|Photo Gallery | The City Mission Intranet
+vti_nexttolasttimemodified:TR|09 Aug 2019 15:08:13 -0000
+vti_cacheddtm:TX|09 Aug 2019 15:08:29 -0000
+vti_filesize:IR|4420
+vti_cachedtitle:SR|Photo Gallery | The City Mission Intranet
+vti_cachedbodystyle:SR|<body>
+vti_cachedlinkinfo:VX|G|../master.dwt S|https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js Q|../styles/style1.css H|http://thecitymission.org S|../images/logo.png H|../ H|../about/about.html H|../newsletters/newsletters.html H|../church_and_community/church_community_home.php H|../classifieds/classifieds.html H|../human_resources/human_resources.php H|../prayer_requests/prayer.html H|staff.html S|../images/Home\\ Portal\\ Banners/photo_gallery_banner.jpg H|https://g.page/thecitymission H|mailto:jbraden@thecitymission.org
+vti_cachedsvcrellinks:VX|FGUS|master.dwt NSSS|https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js FQUS|styles/style1.css NHHS|http://thecitymission.org FSUS|images/logo.png WHUS|index.html FHUS|about/about.html FHUS|newsletters/newsletters.html FHUS|church_and_community/church_community_home.php FHUS|classifieds/classifieds.html FHUS|human_resources/human_resources.php FHUS|prayer_requests/prayer.html FHUS|staff_gallery/staff.html FSUS|images/Home\\ Portal\\ Banners/photo_gallery_banner.jpg NHSS|https://g.page/thecitymission NHUS|mailto:jbraden@thecitymission.org
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type text/html;\\ charset=utf-8 viewport width=device-width,\\ initial-scale=1.0
+vti_charset:SR|utf-8
+vti_syncwith_localhost\\\\\\mra-sql1\\missionnet\\sandbox missionnet///mra-sql1/missionnet/sandbox missionnet:TR|09 Aug 2019 15:08:29 -0000
